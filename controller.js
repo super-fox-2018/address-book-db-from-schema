b2b0e3200e0dc25263ca2c2ef3bc3e259fa1b3f5
handipriyono <handimacbook@gmail.com>
2018-05-16 21:03:38 +0700

    update files complete

--- controller.js
+++ controller.js
@@ -1,19 +1,66 @@
-var modelku = require('./model.js')
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./address_book.db');
+var modelku = require('./model.js');
 modelku = new modelku()
-var view = require('./view.js')
+var view = require('./view.js');
 view = new view()
 
-class Controller {
+class Contacts {
+  constructor() {
+    // this.name = contactData.name
+    // this.company = contactData.company
+    // this.phone = contactData.phone
+    // this.email = contactData.email
+  }
 
 
-
-
-  addcontact(name, company, phone, email) {
-
+  addContactku(name, company, phone, email) {
     modelku.makeContacts(name, company, phone, email)
+  }
 
+
+  save() {
+    //
+    // const savedata = `INSERT INTO Contacts (name, company, phone, email)
+    //              VALUES ('${this.name}', '${this.company}', '${this.phone}','${this.email}')`;
+    // db.run(savedata, function(err) {
+    //   if (err) throw err;
+    // });
 
   }
+
+
+  delete(id) {
+    modelku.deleteContacts(id);
+  }
+
+
+  //
+  //   Update() {
+  //     const update = `UPDATE Contacts SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+  // WHERE CustomerID = 1;`
+  //   }
+
+
+
+  // ---------------------- GROUP -------------------- //
+
+  buatGroup(name) {
+    modelku.makeGroups(name)
+  }
+
+
+  assignGroup(name, group) {
+    modelku.addtoGroup(name, group)
+  }
+
+
+  lihatkontak(username) {
+
+    modelku.showContacts(username)
+
+  }
+
 
 
 
@@ -22,6 +69,17 @@ class Controller {
 
 
 
+//
+//
+// let andi = new Contacts({
+//   name: 'suar',
+//   company: 'Hacktiv81',
+//   phone: '07177670071',
+// })
+// console.log(andi)
+// andi.save()
 
+module.exports = {
+  Contacts
 
-module.exports = Controller
+}
--- model.js
+++ model.js
@@ -7,20 +7,106 @@ class Contacts {
 
   }
 
-
-
   makeContacts(name, company, phone, email) {
     let query = `INSERT INTO Contacts(name,company, phone, email) VALUES ("${name}","${company}", "${phone}", "${email}")`
     db.run(query, function(err) {
       if (err) throw err
+
+    })
+  }
+
+
+  deleteContacts(id) {
+    const deletes = `DELETE FROM Contacts WHERE id = '${id}';`
+    db.run(deletes, function(err) {
+      if (err) throw err;
+    });
+  }
+
+
+  showContacts(username) {
+
+    let lihat = `SELECT * FROM Contacts WHERE name = "${username}"`
+    db.get(lihat, function(err, namacontact) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(namacontact)
+
+    })
+  }
+
+
+
+  makeGroups(name) {
+    let query = `INSERT INTO Groups(name) VALUES ("${name}")`
+    db.run(query, function(err) {
+      if (err) throw err
     })
 
+  }
 
+
+
+
+
+
+  addtoGroup(username, group) {
+
+    //, groupname
+
+    // SELECT * FROM Groups where name = 'HACKTIV8'
+
+
+    let cariid = `SELECT id FROM Contacts WHERE name = "${username}"`
+    db.get(cariid, function(err, namacontact) {
+      if (err) {
+        console.log(err);
+      }
+      let idkontak = namacontact.ID
+
+
+      //cari group ID
+      db.get(`SELECT * FROM Groups where name = '${group}'`, function(err, grup) {
+        if (err) {
+          console.log(err);
+        }
+
+        let groupid = grup.id
+        let tambahgrup = `INSERT INTO contact_group (contact_id, group_id) VALUES ("${idkontak}","${groupid}")`
+        db.run(tambahgrup, function(err) {
+          if (err) throw err
+        })
+
+
+
+
+
+
+      })
+
+
+
+
+
+
+
+
+    })
 
 
 
   }
 
+
+
+
+
 }
+
+
+
+
+
 
 module.exports = Contacts
